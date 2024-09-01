16d1a2773a77f5aea49120bf32820f43db05beb3
himadribh07 <hbhattacharya3@gmail.com>
2024-09-01 21:57:44 +0530

    Primary changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import AuthLayout from "./_auth/AuthLayout";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForms from "./_auth/forms/SignupForms";
+import { Home } from "./_root/pages";
+import RootLayout from "./_root/RootLayout";
 import "./global.css";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello INSTAZEN!</h1>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        //public forms
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForms />} />
+        </Route>
+        //private forms/page
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
---
+++ src/_auth/forms/SignupForms.tsx
@@ -0,0 +1,107 @@
+import { z } from "zod";
+import { Button } from "../../components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { signupValidation } from "@/lib/validation";
+
+const SignupForms = () => {
+  const form = useForm<z.infer<typeof signupValidation>>({
+    resolver: zodResolver(signupValidation),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof signupValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo3.png" width={175} />
+
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Create a new account
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">
+          To use InstaGen enter your details
+        </p>
+      </div>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input type="text" className="shad-input" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input type="text" className="shad-input" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="password" className="shad-input" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" className="shad-input" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="shad-button_primary">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default SignupForms;
